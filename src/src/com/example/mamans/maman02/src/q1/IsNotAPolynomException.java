4372e29ce8d766b97aaa5a33a6da982dbf6e1ee9
omershreib <omershreib@gmail.com>
2026-05-14 22:13:30 +0300

    working on maman02 q1 and q2

--- src/src/com/example/mamans/maman02/src/q1/IsNotAPolynomException.java
+++
@@ -1,7 +0,0 @@
-package com.example.mamans.maman02.src.q1;
-
-public class IsNotAPolynomException extends Exception {
-    public IsNotAPolynomException(String errorMessage) {
-        super(errorMessage);
-    }
-}
